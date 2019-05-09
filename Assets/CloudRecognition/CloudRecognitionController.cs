974d1cddfcbae6ad2cbe143927771e5e4ecd0136
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-05-09 23:41:46 +0200

    it works without gauges

--- Assets/CloudRecognition/CloudRecognitionController.cs
+++ Assets/CloudRecognition/CloudRecognitionController.cs
@@ -37,6 +37,7 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>();
         GameObject friendsCanvas = this.transform.Find("Canvas").GetComponent<Canvas>().gameObject;
         Utility.showHide(friendsCanvas, false);
+        fillFriends();
         if (mCloudRecoBehaviour)
         {
             mCloudRecoBehaviour.RegisterEventHandler(this);
@@ -117,22 +118,47 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         TextMeshPro userName = instance.transform.Find("Canvas/FriendPanel/UserName").GetComponent<TextMeshPro>();
         String name = this.jsonDetectedObject.GetField("name").str;
         userName.text = name;*/
-        RectTransform userPanel = instance.transform.Find("Canvas/UserPanel").GetComponent<RectTransform>();
-        Utility.showHide(userPanel.gameObject, false);
+       /* RectTransform userPanel = instance.transform.Find("Canvas/UserPanel").GetComponent<RectTransform>();
+        Utility.showHide(userPanel.gameObject, false);*/
         GameObject friendsCanvas = this.transform.Find("Canvas").GetComponent<Canvas>().gameObject;
         Utility.showHide(friendsCanvas, true);
         //string str = "{\"usersUrl_Film1\": {\"1\":\"https://drive.google.com/uc?export=download&id=1AZ279LzgZCbfkaasa3eKJzDUi-q0T1CI\",\"2\":\"https://drive.google.com/uc?export=download&id=1Hsoa9hy_RK6ddMdLSIsFXOfiNieS9KRa\",\"3\":\"https://drive.google.com/uc?export=download&id=11_ZRh-t9z1cAOLPaXErg9Uby-r7lxkDL\",\"4\":\"https://drive.google.com/uc?export=download&id=1AWP_iRqBDbbHVe3DugqQH9HrK3g8RgD_\",\"5\":\"https://drive.google.com/uc?export=download&id=1iRUr8ZY-xfgczAw9sWyMclMo_e5JQiIm\",\"6\":\"https://drive.google.com/uc?export=download&id=17qRydHbpDJ444O4ZjFtQVT8Oum7aOeCs\"},\"usersUrl_Film2\": {\"1\":\"https://drive.google.com/uc?export=download&id=1AZ279LzgZCbfkaasa3eKJzDUi-q0T1CI\",\"2\":\"https://drive.google.com/uc?export=download&id=1Hsoa9hy_RK6ddMdLSIsFXOfiNieS9KRa\",\"3\":\"https://drive.google.com/uc?export=download&id=1iRUr8ZY-xfgczAw9sWyMclMo_e5JQiIm\",\"4\":\"https://drive.google.com/uc?export=download&id=1AWP_iRqBDbbHVe3DugqQH9HrK3g8RgD_\",\"5\":\"https://drive.google.com/uc?export=download&id=11_ZRh-t9z1cAOLPaXErg9Uby-r7lxkDL\",\"6\":\"https://drive.google.com/uc?export=download&id=17qRydHbpDJ444O4ZjFtQVT8Oum7aOeCs\"},\"usersUrl_Film3\": {\"1\":\"https://drive.google.com/uc?export=download&id=1AZ279LzgZCbfkaasa3eKJzDUi-q0T1CI\",\"2\":\"https://drive.google.com/uc?export=download&id=1Hsoa9hy_RK6ddMdLSIsFXOfiNieS9KRa\",\"3\":\"https://drive.google.com/uc?export=download&id=11_ZRh-t9z1cAOLPaXErg9Uby-r7lxkDL\",\"4\":\"https://drive.google.com/uc?export=download&id=1AWP_iRqBDbbHVe3DugqQH9HrK3g8RgD_\",\"5\":\"https://drive.google.com/uc?export=download&id=1iRUr8ZY-xfgczAw9sWyMclMo_e5JQiIm\",\"6\":\"https://drive.google.com/uc?export=download&id=17qRydHbpDJ444O4ZjFtQVT8Oum7aOeCs\"},\"films\": {\"1\":\"https://drive.google.com/uc?export=download&id=1JT9Bcl6FimSqvejD3BNvVWmbv0t8mlVE\",\"2\":\"https://drive.google.com/uc?export=download&id=1RBiLM2II3tEXs-ZsdTSWroMu-xCzrbR2\",\"3\":\"https://drive.google.com/uc?export=download&id=1WqyyVvg56pSeCyC0KAtjZLmmekZ0hyi9\"},\"usersRatings_Film1\": {\"1\":8,\"2\":7,\"3\":6,\"4\":5,\"5\":4,\"6\":3},\"usersRatings_Film2\": {\"1\":1,\"2\":1,\"3\":1,\"4\":1,\"5\":1,\"6\":1},\"usersRatings_Film3\": {\"1\":5,\"2\":5,\"3\":5,\"4\":5,\"5\":5,\"6\":5}}";
         string str = "{\"usersUrl_Film1\": {\"1\":\"https://drive.google.com/uc?export=download&id=1u_KQSjgfq6yjEWDgVu26CD3LbOa6LF6I\",\"2\":\"https://drive.google.com/uc?export=download&id=1El8StoIH-90zQS5S3vJ-fm3O0eHXiArC\",\"3\":\"https://drive.google.com/uc?export=download&id=1e_2RqYU-8VNrsjzYTIHJSLe7q7rJzWkQ\",\"4\":\"https://drive.google.com/uc?export=download&id=1txUTitJRfHu1YPftp2db2WIFcwqBEh7n\",\"5\":\"https://drive.google.com/uc?export=download&id=1oKxiawgsZ4HVpMngdLmePqSRYllNJ1bv\",\"6\":\"https://drive.google.com/uc?export=download&id=1BKH2Msrv9LwTLaMZE-VP2FlUqeLUoMtO\"},\"usersUrl_Film2\": {\"1\":\"https://drive.google.com/uc?export=download&id=1u_KQSjgfq6yjEWDgVu26CD3LbOa6LF6I\",\"2\":\"https://drive.google.com/uc?export=download&id=1El8StoIH-90zQS5S3vJ-fm3O0eHXiArC\",\"3\":\"https://drive.google.com/uc?export=download&id=1oKxiawgsZ4HVpMngdLmePqSRYllNJ1bv\",\"4\":\"https://drive.google.com/uc?export=download&id=1txUTitJRfHu1YPftp2db2WIFcwqBEh7n\",\"5\":\"https://drive.google.com/uc?export=download&id=1e_2RqYU-8VNrsjzYTIHJSLe7q7rJzWkQ\",\"6\":\"https://drive.google.com/uc?export=download&id=1BKH2Msrv9LwTLaMZE-VP2FlUqeLUoMtO\"},\"usersUrl_Film3\": {\"1\":\"https://drive.google.com/uc?export=download&id=1u_KQSjgfq6yjEWDgVu26CD3LbOa6LF6I\",\"2\":\"https://drive.google.com/uc?export=download&id=1El8StoIH-90zQS5S3vJ-fm3O0eHXiArC\",\"3\":\"https://drive.google.com/uc?export=download&id=1e_2RqYU-8VNrsjzYTIHJSLe7q7rJzWkQ\",\"4\":\"https://drive.google.com/uc?export=download&id=1txUTitJRfHu1YPftp2db2WIFcwqBEh7n\",\"5\":\"https://drive.google.com/uc?export=download&id=1oKxiawgsZ4HVpMngdLmePqSRYllNJ1bv\",\"6\":\"https://drive.google.com/uc?export=download&id=1BKH2Msrv9LwTLaMZE-VP2FlUqeLUoMtO\"},\"films\": {\"1\":\"https://drive.google.com/uc?export=download&id=1JT9Bcl6FimSqvejD3BNvVWmbv0t8mlVE\",\"2\":\"https://drive.google.com/uc?export=download&id=1RBiLM2II3tEXs-ZsdTSWroMu-xCzrbR2\",\"3\":\"https://drive.google.com/uc?export=download&id=1WqyyVvg56pSeCyC0KAtjZLmmekZ0hyi9\"},\"usersRatings_Film1\": {\"1\":8,\"2\":7,\"3\":6,\"4\":5,\"5\":4,\"6\":3},\"usersRatings_Film2\": {\"1\":1,\"2\":1,\"3\":1,\"4\":1,\"5\":1,\"6\":1},\"usersRatings_Film3\": {\"1\":5,\"2\":5,\"3\":5,\"4\":5,\"5\":5,\"6\":5}}";
+        string definitivo = "[{\"value0\":{\"id\":64,\"creatorId\":3,\"filmId\":5,\"title\":\"Palomitas y leones\",\"date\":\"2019-06-20\",\"location\":\"Principe Pio\",\"description\":\"Muuuuuy prioritario\uD83D\uDE0A\"},\"value1\":{\"userId\":2,\"filmId\":5,\"rating\":10.0,\"date\":\"2019-05-09T20:41:27.510+0000\"},\"value2\":{\"id\":5,\"uuid\":\"b1d63dd282174648a18b207057d9c177\",\"name\":\"El Rey LeÔö£Ôöén\",\"director\":\"Roger Allers\",\"trailerURL\":\"https://www.youtube.com/watch?v=xB5ceAruYrI\",\"infoURL\":null,\"synopsis\":\"Tras la muerte de su padre, Simba vuelve a enfrentar a su malvado tío, Scar, y reclamar el trono de rey.\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/the-lion-king.jpg\",\"genre\":\"Animation\",\"duration\":88,\"rating\":8.5,\"country\":\"USA\",\"premiere\":\"2019-02-07T23:00:00.000+0000\"},\"value3\":[{\"id\":2,\"uuid\":\"4c8048623944436699b3456fad3238d2\",\"name\":\"Diego\",\"email\":\"dacuna@ucm.es\",\"password\":\"1234\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/diego.png\"},{\"id\":1,\"uuid\":\"596abcff71644f4eb855a2d372941674\",\"name\":\"Zihao\",\"email\":\"zhong@ucm.es\",\"password\":\"1234\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/zihao.png\"}],\"size\":4}]";
+
         JSONObject json = new JSONObject(str);
-        StartCoroutine(cargaImagen(json.GetField("films").GetField("1").str, this.transform.Find("Canvas/FirstPosition").GetComponent<UnityEngine.UI.Image>()));
-        StartCoroutine(cargaImagen(json.GetField("films").GetField("2").str, this.transform.Find("Canvas/SecondPosition").GetComponent<UnityEngine.UI.Image>()));
+        JSONObject json_def = new JSONObject(definitivo);
+
+        if (json_def.IsArray && json_def.list.Count > 0 )
+        {
+            Debug.Log("ES UN ARRAY");
+            for (int i = 0; i < json_def.list.Count; i++)
+            {
+                StartCoroutine(cargaImagen(json_def.list[i].GetField("value2").GetField("imageURL").str, this.transform.Find("Canvas/" + i).GetComponent<UnityEngine.UI.Image>()));
+                //Debug.Log(json_def.list[0].GetField("value0").GetField("title"));
+            }
+            for (int i = 0; i < json_def.list[0].GetField("value3").list.Count; i++)
+            {
+                Debug.Log("en el user hay" + json_def.list[0].GetField("value3").list[i].GetField("imageURL").str);
+                StartCoroutine(cargaImagen(json_def.list[0].GetField("value3").list[i].GetField("imageURL").str, this.transform.Find("Canvas/0/Friend" + (i+1)).GetComponent<UnityEngine.UI.Image>()));
+                /*int puntuacion = int.Parse(json.GetField("usersRatings_Film1").GetField(i.ToString()).ToString(), System.Globalization.NumberStyles.Integer);
+                this.transform.Find("Canvas/FirstPosition/Friend" + i + "/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + puntuacion * 10);*/
+            }
+
+        }
+
+
+        if (json_def.HasField("value0")) StartCoroutine(cargaImagen(json.GetField("value0").GetField("1").str, this.transform.Find("Canvas/FirstPosition").GetComponent<UnityEngine.UI.Image>()));
+
+       // StartCoroutine(cargaImagen(json.GetField("films").GetField("1").str, this.transform.Find("Canvas/FirstPosition").GetComponent<UnityEngine.UI.Image>()));
+       /* StartCoroutine(cargaImagen(json.GetField("films").GetField("2").str, this.transform.Find("Canvas/SecondPosition").GetComponent<UnityEngine.UI.Image>()));
         StartCoroutine(cargaImagen(json.GetField("films").GetField("3").str, this.transform.Find("Canvas/ThirdPosition").GetComponent<UnityEngine.UI.Image>()));
         for (int i = 1; i <= 6; i++)
         {
             StartCoroutine(cargaImagen(json.GetField("usersUrl_Film1").GetField(i.ToString()).str, this.transform.Find("Canvas/FirstPosition/Friend" + i).GetComponent<UnityEngine.UI.Image>()));
             int puntuacion = int.Parse(json.GetField("usersRatings_Film1").GetField(i.ToString()).ToString(), System.Globalization.NumberStyles.Integer);
             this.transform.Find("Canvas/FirstPosition/Friend" + i + "/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + puntuacion*10);
-        }
+        }*/
         top = new Dictionary<int, plan_info>();
         for (int i = 1; i <= 3; i++)
         {
