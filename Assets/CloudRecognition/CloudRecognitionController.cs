ec934a150b44bdcd53503497875794fccf1e19a3
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-05-15 15:49:35 +0200

    finish join to plan in AR

--- Assets/CloudRecognition/CloudRecognitionController.cs
+++ Assets/CloudRecognition/CloudRecognitionController.cs
@@ -21,11 +21,13 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
     private String idDetected;
     private String idUser = "";
     private String idFilm = "";
+    private AndroidJavaObject jo;
 
     private struct plan_info
     {
         public ArrayList usersUrl;
-        public int rate;
+        public double rate;
+        public int planID;
     }
     private Dictionary<int, plan_info> top;
     void Start()
@@ -91,6 +93,8 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         Utility.showHide(userPanel.gameObject, false);
         RectTransform friendPanel = instance.transform.Find("Canvas/FriendPanel").GetComponent<RectTransform>();
         Utility.showHide(friendPanel.gameObject, false);
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        this.jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
     }
 
     private void fillUserData() {
@@ -109,6 +113,8 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         GameObject friendsCanvas = this.transform.Find("Canvas").GetComponent<Canvas>().gameObject;
         Utility.showHide(friendsCanvas, true);
         //we load the json with the information that comes from android
+        string definitivo = "[{\"value2\":{\"id\":5,\"uuid\":\"b1d63dd282174648a18b207057d9c177\",\"name\":\"El Rey LeÔö£Ôöén\",\"director\":\"Roger Allers\",\"trailerURL\":\"https://www.youtube.com/watch?v=xB5ceAruYrI\",\"infoURL\":null,\"synopsis\":\"Tras la muerte de su padre, Simba vuelve a enfrentar a su malvado tío, Scar, y reclamar el trono de rey.\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/the-lion-king.jpg\",\"genre\":\"Animation\",\"duration\":88,\"rating\":8.5,\"country\":\"USA\",\"premiere\":\"2019-02-07T23:00:00.000+0000\"},\"value3\":[{\"id\":2,\"uuid\":\"4c8048623944436699b3456fad3238d2\",\"name\":\"Diego\",\"email\":\"dacuna@ucm.es\",\"password\":\"1234\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/diego.png\"},{\"id\":3,\"uuid\":\"6cac53efcd2e4caebcda9ea401d0e782\",\"name\":\"Daniel\",\"email\":\"dacalle@ucm.es\",\"password\":\"1234\",\"imageURL\":\"http://filmar-develop.herokuapp.com/images/carlos.png\"}],\"value0\":{\"id\":64,\"creatorId\":3,\"filmId\":5,\"title\":\"Palomitas y leones\",\"date\":\"2019-06-20\",\"location\":\"Principe Pio\",\"description\":\"Muuuuuy prioritario😊\"},\"value1\":{\"userId\":2,\"filmId\":5,\"rating\":10,\"date\":\"2019-05-12T11:06:11.543+0000\"},\"size\":4}]";
+
         JSONObject json_def = new JSONObject(info);
         //we take and show the information of the recommendation
         if (json_def.IsArray && json_def.list.Count > 0 )
@@ -121,12 +127,12 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
             {
                 StartCoroutine(cargaImagen(json_def.list[0].GetField("value3").list[i].GetField("imageURL").str, this.transform.Find("Canvas/0/Friend" + (i+1)).GetComponent<UnityEngine.UI.Image>()));
             }
-            int rate = int.Parse(json_def.list[0].GetField("value1").GetField("rating").ToString());
-            this.transform.Find("Canvas/0/Friend1/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + rate * 10);
+            double rate = double.Parse(json_def.list[0].GetField("value1").GetField("rating").ToString());
+            this.transform.Find("Canvas/0/Friend1/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + Math.Round(rate) * 10);
 
         }
         //we save the information to change it by pressing the arrows
-        top = new Dictionary<int, plan_info>();
+        this.top = new Dictionary<int, plan_info>();
         for (int i = 0; i < json_def.list.Count; i++)
         {
             plan_info planAux = new plan_info();
@@ -136,15 +142,17 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
                 planAux.usersUrl.Add(json_def.list[i].GetField("value3").list[j].GetField("imageURL").str);
             }
             json_def.list[i].GetField("value1").GetField("rating");
-            planAux.rate = int.Parse(json_def.list[i].GetField("value1").GetField("rating").ToString());
-            top.Add(i, planAux);
+            planAux.rate = double.Parse(json_def.list[i].GetField("value1").GetField("rating").ToString());
+            planAux.planID = int.Parse(json_def.list[i].GetField("value0").GetField("id").ToString());
+            this.top.Add(i, planAux);
         }
-        while(top.Count< 3)
+        while(this.top.Count< 3)
         {
             plan_info planAux = new plan_info();
             planAux.usersUrl = new ArrayList();
             planAux.rate = -1;
-            top.Add(top.Count, planAux);
+            planAux.planID = -1;
+            this.top.Add(this.top.Count, planAux);
         }   
 
     }
@@ -292,6 +300,21 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
     {
         areFriends(info);
     }
+    public void joinPlan()
+    {
+        //method that communicates with android to add a friend
+        using (AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                int id = this.top[0].planID;
+                string id_string = "" + id;
+                Debug.Log("El id es");
+                Debug.Log(id);
+                jo.Call("DAOController", "joinPlan", id_string);
+            }
+        }
+    }
     private void comunication(String method, String _id)
     {
         using (AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
@@ -368,17 +391,17 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         Destroy(image2Aux);
 
         resetUserImages();
-        for (int i = 0; i < top[2].usersUrl.Count; i++)
+        for (int i = 0; i < this.top[2].usersUrl.Count; i++)
         {
-            StartCoroutine(cargaImagen((string)top[2].usersUrl[i], this.transform.Find("Canvas/0/Friend" + (i + 1)).GetComponent<UnityEngine.UI.Image>()));
+            StartCoroutine(cargaImagen((string)this.top[2].usersUrl[i], this.transform.Find("Canvas/0/Friend" + (i + 1)).GetComponent<UnityEngine.UI.Image>()));
         }
         int puntuacion = (int)top[2].rate;
         if(puntuacion != -1) this.transform.Find("Canvas/0/Friend1/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + puntuacion * 10);
         Dictionary<int, plan_info> newDictionary = new Dictionary<int, plan_info>();
-        newDictionary.Add(0, top[2]);
-        newDictionary.Add(1, top[0]);
-        newDictionary.Add(2, top[1]);
-        top = newDictionary;
+        newDictionary.Add(0, this.top[2]);
+        newDictionary.Add(1, this.top[0]);
+        newDictionary.Add(2, this.top[1]);
+        this.top = newDictionary;
     }
     public void friendsRightArrow()
     {
@@ -397,23 +420,23 @@ public class CloudRecognitionController : MonoBehaviour, ICloudRecoEventHandler
         image1.sprite = image2Aux.sprite;
         Destroy(image2Aux);
         resetUserImages();
-        for (int i = 0; i < top[1].usersUrl.Count; i++)
+        for (int i = 0; i < this.top[1].usersUrl.Count; i++)
         {
-            StartCoroutine(cargaImagen((string)top[1].usersUrl[i], this.transform.Find("Canvas/0/Friend" + (i+1)).GetComponent<UnityEngine.UI.Image>()));
+            StartCoroutine(cargaImagen((string)this.top[1].usersUrl[i], this.transform.Find("Canvas/0/Friend" + (i+1)).GetComponent<UnityEngine.UI.Image>()));
         }
-        int puntuacion = (int)top[1].rate;
+        int puntuacion = (int)this.top[1].rate;
         if (puntuacion != -1) this.transform.Find("Canvas/0/Friend1/Gauge").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Gauge/gauge" + puntuacion * 10);
         Dictionary<int, plan_info> newDictionary = new Dictionary<int, plan_info>();
-        Debug.Log("POSICION 0" + top[0].rate);
-        Debug.Log("POSICION 1" + top[1].rate);
-        Debug.Log("POSICION 2" + top[2].rate);
-        newDictionary.Add(0, top[1]);
-        newDictionary.Add(1, top[2]);
-        newDictionary.Add(2, top[0]);
-        top = newDictionary;
-        Debug.Log("POSICION 0" + top[0].rate);
-        Debug.Log("POSICION 1" + top[1].rate);
-        Debug.Log("POSICION 2" + top[2].rate);
+        Debug.Log("POSICION 0" + this.top[0].rate);
+        Debug.Log("POSICION 1" + this.top[1].rate);
+        Debug.Log("POSICION 2" + this.top[2].rate);
+        newDictionary.Add(0, this.top[1]);
+        newDictionary.Add(1, this.top[2]);
+        newDictionary.Add(2, this.top[0]);
+        this.top = newDictionary;
+        Debug.Log("POSICION 0" + this.top[0].rate);
+        Debug.Log("POSICION 1" + this.top[1].rate);
+        Debug.Log("POSICION 2" + this.top[2].rate);
     }
     private void resetUserImages()
     {
